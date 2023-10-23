6ceb5962611e79ab3af7f0e36a1088f8cddecc3a
yotamaruyama <maruyama.youta2002@gmail.com>
2023-10-23 04:43:03 +0000

    10_23_最新版

--- occcupancy_rate_api/socket_code.py
+++ occcupancy_rate_api/socket_code.py
@@ -39,58 +39,49 @@ def main():
     port = 4096
     #ip = "192.168.8.114"
     #port = 1234
-    is_first_run = True
+    #is_first_run = True
 
-    #cnt = 0
-    #is_operatinaol = data_change(msg)
-
-
-    while is_first_run:
+    while True:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 新しいソケットを作成
+
         try:
-            client_socket.settimeout(3)  # タイムアウトを設定
             client_socket.connect((ip,port))
             print("サーバに接続できました。")
-            is_first_run = False
-            client_socket.sendall(bytes(b"500000FF03FF000018002004010000D*0000100001"))
-            """try :
-                response = client_socket.recv(1024).decode()
-            except ConnectionResetError:
-                print("接続がリセットされました。再接続します...")
-                client_socket.close()
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((ip, port))"""
-        except (TimeoutError,ConnectionRefusedError):
+            #client_socket.close()  # ソケットを閉じる
+            break  # ループを抜ける
+            #is_first_run = False
+
+        except ConnectionRefusedError:
             print("サーバに接続できません。再試行します...")
-            time.sleep(3)
-
-        try:
-            while True:
-                one_minutes_timer()
-                try:
-                    #レジスタ読み出し要求(PLCのレジスタはどこを指定するか？→D210)
-                    #D210のレジスタに0xffが格納されているかを、D210の値を読み出して確認
-                    client_socket.send(bytes(b"500000FF03FF00001C002004010000D*0000100001"))
-                    response = str(client_socket.recv(1024).decode())
-                    msg,is_operational = data(response)
-                    base_datetime= datetime.now(pytz.timezone('Asia/Tokyo'))
-                    base_datetime = base_datetime.strftime("%Y-%m-%d %H:%M:%S")
-                    print(f"取得時刻: {base_datetime}")
-                    print(f"サーバからの読み出し応答: {msg}")
-                    print(response)
-                    timestamp = base_datetime
-                    MachineData.objects.create(
-                            timestamp=timestamp,
-                            is_operational=is_operational
-                    )
-
-                except TimeoutError:
-                    print("タイムアウトしました。再試行します...")
-                    time.sleep(3)
-            
-        finally:
             client_socket.close()
-            print("ソケットを閉じる")
+            time.sleep(3)  # 接続が失敗した場合、一定時間待機してから再試行
+    try:
+        while True:
+            one_minutes_timer()
+            try:
+                #レジスタ読み出し要求(PLCのレジスタはどこを指定するか？→D210)
+                #D210のレジスタに0xffが格納されているかを、D210の値を読み出して確認
+                client_socket.send(bytes(b"500000FF03FF000018002004010000D*0000100001"))
+                response = str(client_socket.recv(1024).decode())
+                msg,is_operational = data(response)
+                base_datetime= datetime.now(pytz.timezone('Asia/Tokyo'))
+                base_datetime = base_datetime.strftime("%Y-%m-%d %H:%M:%S")
+                print(f"取得時刻: {base_datetime}")
+                print(f"サーバからの読み出し応答: {msg}")
+                print(response)
+                timestamp = base_datetime
+                MachineData.objects.create(
+                        timestamp=timestamp,
+                        is_operational=is_operational
+                )
+
+            except TimeoutError:
+                print("タイムアウトしました。再試行します...")
+                time.sleep(3)
+        
+    finally:
+        client_socket.close()
+        print("ソケットを閉じる")
                     
 if __name__ == "__main__":
     main()
