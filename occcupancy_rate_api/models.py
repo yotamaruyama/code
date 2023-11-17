ce1c8957fba9094d3ea530c7664acac4f22fbee5
yotamaruyama <maruyama.youta2002@gmail.com>
2023-11-17 00:30:29 +0000

    1117_不要な部分削除

--- occcupancy_rate_api/models.py
+++ occcupancy_rate_api/models.py
@@ -1,8 +1,7 @@
 import socket
 import time
-from datetime import datetime,timedelta,timezone
+from datetime import datetime
 from occupancy_rate.models import MachineData
-import pytz
 
 first_run = True
 
@@ -63,7 +62,7 @@ def main():
             try:
                 #レジスタ読み出し要求(PLCのレジスタはどこを指定するか？→D010)
                 #D010のレジスタに任意の値が格納されているかを、D010の値を読み出して確認
-                client_socket.send(bytes(b"500000FF03FF000018002004010000D*0000100001"))
+                client_socket.sendall(bytes(b"500000FF03FF000018002004010000D*0000100001"))
                 response = str(client_socket.recv(1024).decode())
                 msg,is_operational = data(response)      
                 timestamp= datetime.now()    #日本時間で取得
